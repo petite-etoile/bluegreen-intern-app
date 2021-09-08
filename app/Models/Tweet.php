82cf62c285b4d86379caf4f4a289185e900e0b8c
petite-etoile <tkkydrm3tn@yahoo.co.jp>
2021-09-08 14:43:54 +0900

    make models

---
+++ app/Models/Tweet.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'tweet_text',
+        // 'user_id',
+        // 'root_tweet_id',
+    ];
+}
