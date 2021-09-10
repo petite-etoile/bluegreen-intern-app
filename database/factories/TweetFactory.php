fb97e86107ae66788ab6a7d02388abfcafb6e5b9
petite-etoile <tkkydrm3tn@yahoo.co.jp>
2021-09-10 17:18:26 +0900

    arrange seeder and factory

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -16,15 +16,26 @@ class TweetFactory extends Factory
     protected $model = Tweet::class;
     
     /**
+     * The user index for Tweet's factory
+     *
+     * @var integer
+     */
+    public static $user_id = 0;
+
+    /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition()
-    {
+    {   
+        self::$user_id++;
+        if(self::$user_id > User::count()){
+            self::$user_id = 1;
+        }
         return [
             'tweet_text' => $this->faker->sentence,
-            'user_id' => User::factory(),
+            'user_id' => self::$user_id,
         ];
     }
 }
