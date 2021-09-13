42dc9335bcf5e2383f42d2dd318badc2e7301717
petite-etoile <tkkydrm3tn@yahoo.co.jp>
2021-09-13 15:19:34 +0900

    create user page

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,12 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
+
+
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/user_page.blade.php
+++ resources/views/user_page.blade.php
@@ -5,7 +5,21 @@
 
 @section('content')
 
-Mypage <br>
-id: {{ $user }}
+<div>
+    {{ $user->name }}のページ
+</div>
+
+<br>
+<div class="tweet-table">
+    @forelse ($user->tweets as $tweet)
+        <div class="tweet-cell">
+            <a href="/user/{{ $tweet->user_id }}" class="tweeter">{{ $tweet->name }}</a>
+            <div class="tweet-date">{{ $tweet->created_at }}</div>
+            <div class="tweet-text clear">{{ $tweet->tweet_text }}</div>
+        </div>
+    @empty
+        <p style="margin-top:50px; font-size:30px;"> まだツイートがありません. </p>
+    @endforelse
+</div class="tweet-table">
 
 @endsection
