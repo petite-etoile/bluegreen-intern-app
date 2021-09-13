2753329691be3ebe929bc0a478cf207c90c55549
petite-etoile <tkkydrm3tn@yahoo.co.jp>
2021-09-13 14:41:16 +0900

    add new line at end of file.

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -1,29 +1,29 @@
 <?php
 
- namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
- use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
- class FollowController extends Controller
- {
-     //フォローし, 前いたページにリダイレクトさせる
-     public function add(Request $request){
-         // $follow = FollowService::follow([
-         //     'following_user_id' => Auth::id(),
-         //     'followed_user_id' => Auth::id(),
-         // ]);
+class FollowController extends Controller
+{
+    //フォローし, 前いたページにリダイレクトさせる
+    public function add(Request $request){
+        // $follow = FollowService::follow([
+        //     'following_user_id' => Auth::id(),
+        //     'followed_user_id' => Auth::id(),
+        // ]);
 
 
-         return redirect(url()->previous());
+        return redirect(url()->previous());
+    }
+
+    //アンフォローし, 前いたページにリダイレクトさせる
+    public function delete(Request $request){
+        // $follow = FollowService::unfollow([
+        //     'following_user_id' => Auth::id(),
+        //     'followed_user_id' => Auth::id(),
+        // ]);
+
+        return redirect(url()->previous());
      }
-
-     //アンフォローし, 前いたページにリダイレクトさせる
-     public function delete(Request $request){
-         // $follow = FollowService::unfollow([
-         //     'following_user_id' => Auth::id(),
-         //     'followed_user_id' => Auth::id(),
-         // ]);
-
-         return redirect(url()->previous());
-     }
- }
+}
